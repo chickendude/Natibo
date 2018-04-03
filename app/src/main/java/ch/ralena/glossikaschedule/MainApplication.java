11cd667ec5a1486e8c51a3f368a3325e68b93b8e
chickendude <pokemaster103@gmail.com>
2018-04-02 23:16:48 -0700

    Remove Stetho-Realm and Stetho as couldn't get them to work anymore.

--- app/src/main/java/ch/ralena/glossikaschedule/MainApplication.java
+++ app/src/main/java/ch/ralena/glossikaschedule/MainApplication.java
@@ -2,9 +2,6 @@ package ch.ralena.glossikaschedule;
 
 import android.app.Application;
 
-import com.facebook.stetho.Stetho;
-import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
-
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
@@ -15,13 +12,6 @@ public class MainApplication extends Application {
 		super.onCreate();
 		// initialize Realm
 		Realm.init(this);
-
-		Stetho.initialize(
-				Stetho.newInitializerBuilder(this)
-						.enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
-						.enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
-						.build());
-
 		RealmConfiguration config = new RealmConfiguration.Builder()
 				.name("glossikaschedule.realm")
 				.schemaVersion(1)
