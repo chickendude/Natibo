01a57f3ecdb55766304c09b1137b36adfb9c9021
chickendude <pokemaster103@gmail.com>
2018-04-10 22:06:26 -0700

    Added interaction with the radio buttons on the AI course fragment.

--- app/src/main/java/ch/ralena/glossikaschedule/fragment/CourseAiCreateFragment.java
+++ app/src/main/java/ch/ralena/glossikaschedule/fragment/CourseAiCreateFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import ch.ralena.glossikaschedule.R;
 import io.realm.Realm;
@@ -17,12 +20,42 @@ public class CourseAiCreateFragment extends Fragment {
 
 	private Realm realm;
 
+	// Views
+	EditText customScheduleEdit;
+	RadioGroup reviewScheduleRadioGroup;
+	RadioButton fourDayRadio;
+	RadioButton fiveDayRadio;
+	RadioButton customDayRadio;
+
 	@Nullable
 	@Override
 	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_course_ai_create, container, false);
 
 		realm = Realm.getDefaultInstance();
+
+		// get views
+		customScheduleEdit = view.findViewById(R.id.customScheduleEdit);
+		reviewScheduleRadioGroup = view.findViewById(R.id.reviewScheduleRadioGroup);
+		fourDayRadio = view.findViewById(R.id.fourDayRadio);
+		fiveDayRadio = view.findViewById(R.id.fiveDayRadio);
+		customDayRadio = view.findViewById(R.id.customDayRadio);
+
+		// custom schedule edit should start off gone
+		customScheduleEdit.setVisibility(View.GONE);
+
+		// set up radio listeners
+		reviewScheduleRadioGroup.clearCheck();
+		reviewScheduleRadioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+			switch (checkedId) {
+				case R.id.customDayRadio:
+					customScheduleEdit.setVisibility(View.VISIBLE);
+					break;
+				default:
+					customScheduleEdit.setVisibility(View.GONE);
+					break;
+			}
+		});
 
 		return view;
 	}
