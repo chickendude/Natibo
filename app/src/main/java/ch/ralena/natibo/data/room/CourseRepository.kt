f25fdf21a82439c4868902774d5571bd061b0123
chickendude <pokemaster103@gmail.com>
2021-03-30 20:52:29 -0700

    Add return type to fetchCourses function.

--- app/src/main/java/ch/ralena/natibo/data/room/CourseRepository.kt
+++ app/src/main/java/ch/ralena/natibo/data/room/CourseRepository.kt
@@ -2,8 +2,9 @@ package ch.ralena.natibo.data.room
 
 import ch.ralena.natibo.data.room.`object`.Course
 import io.realm.Realm
+import io.realm.RealmResults
 import javax.inject.Inject
 
 class CourseRepository @Inject constructor(private val realm: Realm) {
-	fun fetchCourses() = realm.where(Course::class.java).findAll()
+	fun fetchCourses(): RealmResults<Course> = realm.where(Course::class.java).findAll()
 }
