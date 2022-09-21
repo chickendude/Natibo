10492da34de11cb7ffa90c23c0e2c8565df30e73
chickendude <pokemaster103@gmail.com>
2022-09-21 08:02:46 -0400

    Show sentences on overview screen

--- app/src/main/java/ch/ralena/natibo/ui/study/overview/StudySessionOverviewFragment.kt
+++ app/src/main/java/ch/ralena/natibo/ui/study/overview/StudySessionOverviewFragment.kt
@@ -2,13 +2,16 @@ package ch.ralena.natibo.ui.study.overview
 
 import android.view.View
 import android.widget.TextView
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import ch.ralena.natibo.R
+import ch.ralena.natibo.data.LanguageData.languages
 import ch.ralena.natibo.data.room.`object`.CourseRoom
 import ch.ralena.natibo.data.room.`object`.LanguageRoom
 import ch.ralena.natibo.data.room.`object`.SentenceGroup
@@ -17,8 +20,12 @@ import ch.ralena.natibo.model.NatiboSentence
 import ch.ralena.natibo.model.NatiboSession
 import ch.ralena.natibo.ui.MainActivity
 import ch.ralena.natibo.ui.base.BaseFragment
+import ch.ralena.natibo.ui.study.insession.StudySessionFragment.Companion.KEY_COURSE_ID
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.ArrayList
+
+// Constants
+private val LANGUAGE_WIDTH = 35.dp
 
 @AndroidEntryPoint
 class StudySessionOverviewFragment :
@@ -34,6 +41,7 @@ class StudySessionOverviewFragment :
 		activity = requireActivity() as MainActivity
 		activity.title = getString(R.string.session_overview)
 		// load schedules from database
+		viewModel.registerListener(this)
 		val id = requireArguments().getLong(KEY_COURSE_ID)
 		viewModel.getSentences(id)
 	}
@@ -62,13 +70,33 @@ fun SessionOverview(
 ) {
 	LazyColumn {
 		items(sentences) { sentence ->
+			Sentence(sentence, languages.first, languages.second)
+		}
+	}
+}
+
+@Composable
+fun Sentence(
+	sentence: NatiboSentence,
+	nativeLanguage: LanguageRoom?,
+	targetLanguage: LanguageRoom?
+) {
+	Row(modifier = Modifier.padding(5.dp)) {
+		Text(
+			modifier = Modifier.padding(end = 8.dp),
+			text = sentence.native.index.toString()
+		)
+		Column {
 			Row {
-				Text(text = languages.first?.code ?: "NL")
+				Text(modifier = Modifier.width(LANGUAGE_WIDTH), text = nativeLanguage?.code ?: "NL")
 				Text(text = sentence.native.original)
 			}
 			sentence.target?.original?.let { targetSentence ->
 				Row {
-					Text(text = languages.second?.code ?: "TL")
+					Text(
+						modifier = Modifier.width(LANGUAGE_WIDTH),
+						text = targetLanguage?.code ?: "TL"
+					)
 					Text(text = targetSentence)
 				}
 			}
--- app/src/main/java/ch/ralena/natibo/ui/study/overview/StudySessionOverviewViewModel.kt
+++ app/src/main/java/ch/ralena/natibo/ui/study/overview/StudySessionOverviewViewModel.kt
@@ -3,45 +3,53 @@ package ch.ralena.natibo.ui.study.overview
 import ch.ralena.natibo.data.NatiboResult
 import ch.ralena.natibo.data.room.CourseRepository
 import ch.ralena.natibo.data.room.LanguageRepository
-import ch.ralena.natibo.data.room.SentenceRepository
-import ch.ralena.natibo.data.room.SessionRepository
 import ch.ralena.natibo.data.room.`object`.CourseRoom
 import ch.ralena.natibo.data.room.`object`.LanguageRoom
 import ch.ralena.natibo.model.NatiboSession
 import ch.ralena.natibo.ui.base.BaseViewModel
 import ch.ralena.natibo.usecases.data.FetchSessionWithSentencesUseCase
+import ch.ralena.natibo.utils.DispatcherProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class StudySessionOverviewViewModel @Inject constructor(
 	private val courseRepository: CourseRepository,
 	private val languageRepository: LanguageRepository,
 	private val fetchSessionWithSentencesUseCase: FetchSessionWithSentencesUseCase,
+	private val dispatchers: DispatcherProvider
 ) : BaseViewModel<StudySessionOverviewViewModel.Listener>() {
 	interface Listener {
 		fun sessionLoaded(session: NatiboSession, languages: Pair<LanguageRoom?, LanguageRoom?>)
 	}
 
-	private var course: CourseRoom? = null
 	private val coroutineScope = CoroutineScope(Dispatchers.IO)
 	lateinit var session: NatiboSession
 
 	fun getSentences(courseId: Long) {
 		coroutineScope.launch {
 			val result = courseRepository.fetchCourse(courseId)
-			if (result is NatiboResult.Success) {
-				course = result.data
-				val nativeLanguage = languageRepository.fetchLanguage(course!!.nativeLanguageId)
-				val targetLanguage =
-					course?.targetLanguageId?.let { languageRepository.fetchLanguage(it) }
-				fetchSessionWithSentencesUseCase.fetchSessionWithSentences(result.data.sessionId)
-					?.let { session ->
-						listeners.forEach { it.sessionLoaded(session, Pair(nativeLanguage, targetLanguage)) }
-					}
+			when (result) {
+				is NatiboResult.Success -> fetchSentencesFromCourse(result.data)
+				else -> Unit
 			}
 
+		}
+	}
+
+	private suspend fun fetchSentencesFromCourse(course: CourseRoom) {
+		val nativeLanguage = languageRepository.fetchLanguage(course.nativeLanguageId)
+		val targetLanguage =
+			course.targetLanguageId?.let { languageRepository.fetchLanguage(it) }
+		val session = fetchSessionWithSentencesUseCase.fetchSessionWithSentences(course.sessionId)
+		if (session != null) {
+			withContext(dispatchers.main()) {
+				listeners.forEach {
+					it.sessionLoaded(session, Pair(nativeLanguage, targetLanguage))
+				}
+			}
 		}
 	}
 }
